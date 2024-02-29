4d862d229becb59fe4c401f2e7e2b9d0fe910f1a
Ayan Pakhira <pakhiraayan@gmail.com>
2024-02-29 21:23:58 +0530

    created ccourses

---
+++ components/file-upload.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+import { ourFileRouter } from "@/app/api/uploadthing/core";
+
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  endpoint: keyof typeof ourFileRouter;
+}
